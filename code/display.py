30991ba9babd51975afcba4bd81f1105e742849a
aimalz <aimalz@nyu.edu>
2015-10-02 13:59:11 -0400

    plots now accommodate multi-band data

--- code/display.py
+++ code/display.py
@@ -13,36 +13,45 @@ saveFrames = False # For making movies
 if saveFrames:
 	os.system('rm Frames/*.png')
 
-posterior_sample = atleast_2d(loadtxt('posterior_sample.txt'))
-data = loadtxt('Data/test_image.txt')
-sig = loadtxt('Data/test_sigma.txt')
+posterior_sample = atleast_2d(loadtxt('sample.txt'))
+data = np.reshape(loadtxt('Data/test_image.txt'),(2,200,200))
+print(np.shape(data))
+sig = np.reshape(loadtxt('Data/test_sigma.txt'),(2,200,200))
+print(np.shape(sig))
 
 ion()
 hold(False)
-for i in xrange(0, posterior_sample.shape[0]):
-	img = posterior_sample[i, 0:200**2].reshape((200, 200))
-	subplot(1, 2, 1)
-	imshow(stretch(img), interpolation='nearest', cmap='afmhot')
-	title('Model {i}'.format(i=i))
-	gca().set_xticks([-0.5, 99.5, 199.5])
-	gca().set_yticks([-0.5, 99.5, 199.5])
-	gca().set_xticklabels(['-1', '0', '1'])
-	gca().set_yticklabels(['1', '0', '-1'])
+print(np.shape(posterior_sample))
+print(np.shape(posterior_sample[0, 0:200**2].reshape((200, 200))))
+for j in xrange(0,np.shape(data)[0]):
+  print(j)
+  print(np.shape(posterior_sample[0, j*200**2:(j+1)*200**2]))
+  print(np.shape(posterior_sample[0, j*200**2:(j+1)*200**2].reshape((200, 200))))
+  img = posterior_sample[0, j*200**2:(j+1)*200**2].reshape((200, 200))
+  for i in xrange(0, posterior_sample.shape[0]):
+	  #img = posterior_sample[0, j*200**2:(j+1)*200**2].reshape((200, 200))
+	  subplot(1, 2, 1)
+	  imshow(stretch(img), interpolation='nearest', cmap='afmhot')
+	  title('Model {i}'.format(i=i))
+	  gca().set_xticks([-0.5, 99.5, 199.5])
+	  gca().set_yticks([-0.5, 99.5, 199.5])
+	  gca().set_xticklabels(['-1', '0', '1'])
+	  gca().set_yticklabels(['1', '0', '-1'])
 
-	subplot(1, 2, 2)
-	imshow(img - data, interpolation='nearest', cmap='gray')
-	title('Standardised Residuals')
-	gca().set_xticks([-0.5, 99.5, 199.5])
-	gca().set_yticks([-0.5, 99.5, 199.5])
-	gca().set_xticklabels(['-1', '0', '1'])
-	gca().set_yticklabels(['1', '0', '-1'])
+	  subplot(1, 2, 2)
+	  imshow(img - data[j], interpolation='nearest', cmap='gray')
+	  title('Standardised Residuals')
+	  gca().set_xticks([-0.5, 99.5, 199.5])
+	  gca().set_yticks([-0.5, 99.5, 199.5])
+	  gca().set_xticklabels(['-1', '0', '1'])
+	  gca().set_yticklabels(['1', '0', '-1'])
 
-	draw()
+	  draw()
 
-	if saveFrames:
-		savefig('Frames/' + '%0.4d'%(i+1) + '.png', bbox_inches='tight')
-		print('Frames/' + '%0.4d'%(i+1) + '.png')
+	  if saveFrames:
+		  savefig('Frames/' + '%0.4d'%(i+1) + '.png', bbox_inches='tight')
+		  print('Frames/' + '%0.4d'%(i+1) + '.png')
 
-ioff()
-show()
+  ioff()
+  show()
 
