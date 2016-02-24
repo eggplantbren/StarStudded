8f2c4843d20c4cbcff36ec18396e5eb5b51391a1
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-02-25 12:58:45 +1300

    Removed interactive plotting stuff, works with Anaconda Python

--- code/display.py
+++ code/display.py
@@ -1,59 +1,40 @@
 import matplotlib as mpl
-import colormaps as cmaps
 from pylab import *
 import os
 
-mpl.use('TkAgg')
-
 # Piecewise linear stretch
 def stretch(x):
-    y = x.copy()
-#   y = (y - y.min())/(y.max() - y.min())
-#   y[y > 0.1] = 0.1 + 0.05*(y[y > 0.1] - 0.1)
-    return log(abs(y) + 1.)
+    return log(abs(x) + 1.)
 
-saveFrames = False  # For making movies
-if saveFrames:
-    os.system('rm Frames/*.png')
+os.system('rm Frames/*.png')
 
-metadata = np.loadtxt('Data/test_metadata.txt').astype(int)
-# print(metadata)
+# Load files (DNest4 output and data files)
+metadata = np.loadtxt('Data/test_metadata.txt')
+num_images = int(metadata[0])
+ni = int(metadata[1])
+nj = int(metadata[2])
 
-num_images = metadata[0]
-ni = metadata[1]
-nj = metadata[2]
-
-# num_sam_acccepted X nbands*xdim*ydim
 posterior_sample = atleast_2d(loadtxt('posterior_sample.txt'))
-# print(np.shape(posterior_sample))
-
-# generate after running showresults.py
-# posterior_sample = atleast_2d(loadtxt('posterior_sample.txt'))
-# print(np.shape(posterior_sample))
-# posterior_sample show have same shape as _image.txt
-
 data = np.reshape(loadtxt('Data/test_image.txt'), (num_images, ni, nj))
-print((data.shape))
 sig = np.reshape(loadtxt('Data/test_sigma.txt'), (num_images, ni, nj))
 
-ion()
-hold(False)
-
+figure(figsize=(12, 12))
 for i in range(0, posterior_sample.shape[0]):
     for j in range(0, num_images):
         subplot(num_images, 2, 1 + j*num_images)
         img = posterior_sample[i, j * ni * nj:(j + 1) * ni * nj].reshape((ni, nj))
-        imshow(img, interpolation='nearest', cmap=cmaps.viridis)
+        imshow(img, interpolation='nearest', cmap='viridis')
         title('Model {i}'.format(i=(i+1)))
+        gca().set_xticks([])
+        gca().set_yticks([])
 
         subplot(num_images, 2, 2 + j*num_images)
         imshow((img - data[j, :, :]), interpolation='nearest', cmap='coolwarm')
         title('Residuals')
-    draw()
+        gca().set_xticks([])
+        gca().set_yticks([])
 
-    if saveFrames:
-        savefig('Frames/' + '%0.4d' % (i + 1) + '.png', bbox_inches='tight')
-        print('Frames/' + '%0.4d' % (i + 1) + '.png')
-
-ioff()
+    savefig('Frames/' + '%0.6d' % (i + 1) + '.png', bbox_inches='tight')
+    print('Saved Frames/' + '%0.6d' % (i + 1) + '.png')
 show()
+
