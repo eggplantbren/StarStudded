f5e6aa1e0817e064631694415b413f71c19c0e7b
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-03-30 13:54:14 +1300

    Plot stars

--- code/display.py
+++ code/display.py
@@ -19,15 +19,24 @@ posterior_sample = atleast_2d(dn.my_loadtxt('posterior_sample.txt'))
 data = np.reshape(loadtxt('Data/test_image.txt'), (num_images, ni, nj))
 sig = np.reshape(loadtxt('Data/test_sigma.txt'), (num_images, ni, nj))
 
+stars = posterior_sample[:,30009:(30009 + 300*(2 + num_images))]
+stars_x = stars[:,   0:300]
+stars_y = stars[:, 300:600]
+
 figure(figsize=(12, 12))
 for i in range(0, posterior_sample.shape[0]):
     for j in range(0, num_images):
         subplot(num_images, 2, 1 + 2*j)
         img = posterior_sample[i, j * ni * nj:(j + 1) * ni * nj].reshape((ni, nj))
         try:
-            imshow(img, interpolation='nearest', cmap='viridis')
+            imshow(img, extent=metadata[3:7], interpolation='nearest', cmap='viridis')
         except:
-            imshow(img, interpolation='nearest', cmap='Blues')
+            imshow(img, extent=metadata[3:7], interpolation='nearest', cmap='Blues')
+
+        which = stars_x[i, :] != 0.0
+        plot(stars_x[i, which], stars_y[i, which], 'wo', alpha=0.25)
+        axis(metadata[3:7])
+
         title('Model {i}'.format(i=(i+1)))
         gca().set_xticks([])
         gca().set_yticks([])
